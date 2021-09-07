3100c652f07c8c1976c225c1acc9a76db948caf2
Ahmet Gürsel <ahmetgursel@protonmail.com>
2021-09-08 00:37:15 +0300

    credentials fix

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'eu-cdbr-west-01.cleardb.com';
-$user = 'b29035683a8053';
-$pass = '4f0465a7';
-$db_name = 'heroku_8dabd51d394d90a';
+$host = '';
+$user = '';
+$pass = '';
+$db_name = '';
 
 $conn = new MySQLi($host, $user, $pass, $db_name);
 $conn->set_charset("utf8");
